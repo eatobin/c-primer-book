f8f131ccb1820e5b77ec1f1889d20052c131033a
Eric Tobin <eatobin@gmail.com>
2021-08-12 17:12:26 -0500

    ex_2_4

---
+++ Ch02/ex_2_4.c
@@ -0,0 +1,26 @@
+// ex_2_4.c
+
+#include <stdio.h>
+
+void jolly(void);
+void deny(void);
+
+int main(void)
+{
+    jolly();
+    jolly();
+    jolly();
+    deny();
+
+    return 0;
+}
+
+void jolly(void)
+{
+    printf("For he's a jolly good fellow!\n");
+}
+
+void deny(void)
+{
+    printf("Which nobody can deny!\n");
+}
