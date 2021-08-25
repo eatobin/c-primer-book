f9087162889191aee196408ea8869408312d1158
Eric Tobin <eatobin@gmail.com>
2021-08-25 18:14:20 -0500

    formatting

--- Ch02/butler/two_func.c
+++ Ch02/butler/two_func.c
@@ -1,7 +1,7 @@
 //* two_func.c -- a program using two functions in one file */
 #include <stdio.h>
 void butler(void); /* ANSI/ISO C function prototyping */
-int main(void) {
+int  main(void) {
   printf("I will summon the butler function.\n");
   butler();
   printf("Yes. Bring me some tea and writeable DVDs.\n");
--- Ch02/fathm_ft.c
+++ Ch02/fathm_ft.c
@@ -5,7 +5,7 @@ int main(void) {
   int feet, fathoms;
 
   fathoms = 2;
-  feet = 6 * fathoms;
+  feet    = 6 * fathoms;
   printf("There are %d feet in %d fathoms!\n", feet, fathoms);
   printf("Yes, I said %d feet!\n", 6 * fathoms);
 
