d8cd015362b9c6ac34bed2bbc1f8a56e5c1d5997
Eric Tobin <eatobin@gmail.com>
2021-09-02 16:44:29 -0500

    arraymindynamic.c

--- Ch00/arraymindynamic.c
+++ Ch00/arraymindynamic.c
@@ -5,18 +5,26 @@
 
 int main(void) {
   int *candidates;
-  candidates = malloc(15 * sizeof(*candidates));
-  //  int    candidates[] = {22, 13, 666, 42, 9000, 20};
-  //    int    currentMin   = candidates[0];
-  size_t n = sizeof(candidates);
-  //
-  //  for (int i = 0; i < n; ++i) {
-  //    if (candidates[i] < currentMin) currentMin = candidates[i];
-  //  }
-  //
-  printf("length of candidates: %zu\n", n);
-  //  printf("The min is: %d", currentMin);
-  //
+  int  currentMin;
+
+  candidates = malloc(5 * sizeof(*candidates));
+
+  if (candidates == NULL) {
+    fprintf(stderr, "malloc failed\n");
+    return -1;
+  }
+
+  printf("Enter 5 integers: ");
+  scanf("%d %d %d %d %d", &candidates[0], &candidates[1], &candidates[2],
+        &candidates[3], &candidates[4]);
+
+  currentMin = candidates[0];
+
+  for (int i = 0; i < 5; ++i) {
+    if (candidates[i] < currentMin) currentMin = candidates[i];
+  }
+
+  printf("The min is: %d", currentMin);
   free(candidates);
-  //  return 0;
+  return 0;
 }
