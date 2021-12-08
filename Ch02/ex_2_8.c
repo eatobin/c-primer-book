1d4b89516fdd25d1b6e9544db91e66a8866613a5
Eric Tobin <eatobin@gmail.com>
2021-12-08 17:15:36 -0600

    clean up

--- Ch02/ex_2_8.c
+++ Ch02/ex_2_8.c
@@ -5,18 +5,23 @@
 void one_three(void);
 void two(void);
 
-int main(void) {
-  printf("Starting now:\n");
-  one_three();
-  printf("Done!\n");
+int main(void)
+{
+    printf("Starting now:\n");
+    one_three();
+    printf("Done!\n");
 
-  return 0;
+    return 0;
 }
 
-void one_three(void) {
-  printf("One\n");
-  two();
-  printf("Three\n");
+void one_three(void)
+{
+    printf("One\n");
+    two();
+    printf("Three\n");
 }
 
-void two(void) { printf("Two\n"); }
+void two(void)
+{
+    printf("Two\n");
+}
